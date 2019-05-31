7874ba1115b2c496929e0154194290362266b9b0
Gargi.kantesaria <Gargi.kantesaria@solutionanalysts.com>
2019-05-31 13:37:31 +0530

    initial commit for object detection from image

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, NavParams, AlertController } from 'ionic-angular';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ImageProvider } from '../../providers/image/image';
+import { Camera } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,68 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public addItemsForm: FormGroup;
+  public imageSet: boolean = false;
+  public imgPreview;
+  public itemPicturesStoreURL;
+  public imageObjects = [];
 
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController,
+    public imageProvider: ImageProvider, private camera: Camera) {
+    this.addItemsForm = new FormGroup({
+      'itemTitle': new FormControl('', Validators.required),
+    })
+    this.imgPreview = "/assets/imgs/logo.png";
+  }
+
+  getPhoto() {
+    this.alertCtrl.create({
+      title: 'Profile Picture',
+      message: 'From where do you want to choose your item pic?',
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => { }
+        },
+        {
+          text: 'Choose from gallery',
+          handler: () => {
+            // Call imageProvider to process, upload, and update user photo.
+            this.imageProvider.setProfilePhoto('', this.camera.PictureSourceType.PHOTOLIBRARY).then(data => {
+              this.imgPreview = data;
+              this.imageSet = true;
+            });
+          }
+        },
+        {
+          text: 'Take my photo',
+          handler: () => {
+            // Call imageProvider to process, upload, and update user photo.
+            this.imageProvider.setProfilePhoto('', this.camera.PictureSourceType.CAMERA).then(data => {
+              this.imgPreview = data;
+              this.imageSet = true;
+            });
+          }
+        }
+      ]
+    }).present();
+  }
+
+  submitForm() {
+    if (this.imageSet) {
+      let imageName = 'TestingImage';
+      this.imageProvider.uploadImage(this.imgPreview, imageName).then((res: any) => {
+        this.itemPicturesStoreURL = res;
+        this.imageProvider.getObjectRecognition(res.Bucket, res.Key).then((response:any) => {
+          this.imageObjects = [...response.Labels];
+        }).catch((error) => {
+          console.log("error detection", error);
+        });
+
+      }).catch((err) => {
+        console.log("Error is", err)
+      })
+    }
   }
 
 }
---
+++ src/providers/image/image.ts
@@ -0,0 +1,95 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import * as AWS from 'aws-sdk';
+
+@Injectable()
+export class ImageProvider {
+
+  private options: CameraOptions = {
+    targetWidth: 384,
+    targetHeight: 384,
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+
+  constructor(private camera: Camera) {
+    AWS.config.accessKeyId = 'AKIAIRKREWE3WERITBUA';
+    AWS.config.secretAccessKey = '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';
+    AWS.config.region = 'us-east-1';
+    AWS.config.signatureVersion = 'v4';
+  }
+
+  setProfilePhoto(name, sourceType): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.options.sourceType = sourceType;
+      this.camera.getPicture(this.options).then((res) => {
+        let base64Image = 'data:image/jpeg;base64,' + res;
+        resolve(base64Image);
+      }).catch((err) => {
+        reject(err);
+      })
+    })
+  }
+
+  uploadImage(image, imageName) {
+    return new Promise((resolve, reject) => {
+
+      const body = Buffer.from(image.replace(/^data:image\/\w+;base64,/, ''), 'base64');
+      const ext = image.split(';')[0].split('/')[1] || 'jpg';
+      let date = Date.now();
+      const key = imageName + date;
+
+      this.s3Putimage({ body, mime: `image/${ext}` }, key, 'base64').then((result) => { resolve(result); }).catch((err) => { reject(err); });
+    })
+  }
+
+  s3Putimage(file, key, encoding) {
+    return new Promise((resolve, reject) => {
+      
+      let s3 = new AWS.S3();
+
+      const params = {
+        Body: file.body,
+        Bucket: 'ionicdemoapp',
+        Key: key,
+        ACL: "public-read",
+      };
+
+      s3.upload(params, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    })
+  }
+
+  getObjectRecognition(bucketName, imageUrl) {
+    return new Promise((resolve, reject) => {
+      let rekognition = new AWS.Rekognition();
+
+      let imageParams = {
+        Image: {
+          S3Object: {
+            Bucket: bucketName,
+            Name: imageUrl
+          }
+        },
+        MaxLabels: 123,
+        MinConfidence: 80
+      };
+
+      rekognition.detectLabels(imageParams, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      })
+    })
+  }
+
+}
